e672a2d9df40849e7da6af7b456a523c8a1c69ff
blank138 <blank118@me.com>
2013-09-19 20:15:08 +0200

    created a Rails app about Jetzun Milaripa

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Milarepa::Application.config.secret_key_base = '66db531f4d2673b36028142d7ff053d0c57033f2cda91e12b061902e1dcfeedc09c018854f1df998daae64d55b7d0d31b22682550f067edc64b0e2ea083f008e'
